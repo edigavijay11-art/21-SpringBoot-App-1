4b50d0ad3376efd6a79a47ad5f13a1e1fd99504e
Vijay kumar <edigavijay11@gmail.com>
2026-01-06 17:05:31 +0530

    HIS-134 fix provided

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -10,7 +10,8 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = 
 		SpringApplication.run(Application.class, args);
-		
+		//Changes for HHIS-134
+
 		run.close();
 	}
 
--- src/main/java/in/ashokit/beans/Car.java
+++ src/main/java/in/ashokit/beans/Car.java
@@ -2,8 +2,9 @@ package in.ashokit.beans;
 
 public class Car {
 
-	
-
+	public void start() {
+		//Changes for HHIS-122
+	}
 
 	
 	 
